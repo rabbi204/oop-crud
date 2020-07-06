e656b912e47a56f92ad94aaa7aa139e55a587ac3
rabbi204 <md.rabbi.web@gmail.com>
2020-07-06 21:35:02 +0600

    Database class setup

---
+++ app/Support/Database.php
@@ -0,0 +1,27 @@
+<?php 
+	
+	namespace App\Support;
+
+	abstract class Database 
+	{
+
+		/**
+		 * Server related property
+		 */
+		private $host = 'localhost';
+		private $user = 'root';
+		private $pass = 'root';
+		private $db = 'oop_crud';
+
+		private $connection;
+
+		/**
+		 * Database connection setup
+		 */
+		private function connection()
+		{
+			return $this -> connection = new mysqli($this -> host,$this -> user,$this -> pass,$this -> db);
+		}
+
+
+	}
